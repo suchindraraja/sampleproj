575a05c87c08630274ec05129bd45eb0882727d8
sukumar raja <sukumar raja@sukumar123>
2020-09-10 22:40:45 +0530

    updated

--- src/mypackage/MyClass.java
+++ src/mypackage/MyClass.java
@@ -1,9 +1,11 @@
 package mypackage;
 
-public class MyClass {
+public class MyClass 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi Git");
 
 	}
 
