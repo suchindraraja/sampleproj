6f1fb543512782c8f316c0e16eaaee5818ba9b52
suchindraraja <66240516+suchindraraja@users.noreply.github.com>
2020-09-15 23:05:18 +0530

    Update MyClass.java

--- src/mypackage/MyClass.java
+++ src/mypackage/MyClass.java
@@ -5,7 +5,7 @@ public class MyClass
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hi Git babu, how r u?, I'm fine");
+		System.out.println("Hi Git baby, how r u?, I'm fine");
 
 	}
 
